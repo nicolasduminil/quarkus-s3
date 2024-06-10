fe6dfed1404f66387e903e16c53a764876f49ed7
Nicolas DUMINIL <nicolas.duminil@simplex-software.fr>
2024-06-10 16:09:15 +0200

    testing ...

--- src/main/java/fr/simplex_software/aws/iac/cdk/CdkApiGatewayStack.java
+++ src/main/java/fr/simplex_software/aws/iac/cdk/CdkApiGatewayStack.java
@@ -9,14 +9,8 @@ import software.amazon.awscdk.services.s3.*;
 @ApplicationScoped
 public class CdkApiGatewayStack extends Stack
 {
-  private static final String HANDLER = "io.quarkus.amazon.lambda.runtime.QuarkusStreamHandler::handleRequest";
-  private static int RAM = 128;
-  private static int TIME_OUT = 10;
-  private static final String FUNCTION = "QuarkusApiGatewayLambda";
-  private static final String ID = "quarkus-api-gateway-lambda";
-
   @Inject
-  public CdkApiGatewayStack(final App scope, @ConfigProperty(name = "cdk.stack-id", defaultValue = "QuarkusApiGatewayStack") final String stackId, final StackProps props)
+  public CdkApiGatewayStack(final App scope, @ConfigProperty(name = "cdk.stack-id", defaultValue = "QuarkusStack") final String stackId, final StackProps props)
   {
     super(scope, stackId, props);
     Bucket.Builder.create(this, "myBucket").build();
